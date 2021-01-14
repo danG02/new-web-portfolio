c93c61e8e010f063330ce8a5ad88f24fa55625de
Danny <danny20012011@icloud.com>
2021-01-13 19:56:44 -0500

    Made another project showcase, need to make little changes and fix code

--- project.js
+++ project.js
@@ -1,9 +1,13 @@
 const triggerFlip = document.querySelector('.Card')
-//const box = document.querySelector('.Container')
+const box2 = document.querySelector('#box2')
 
 
 triggerFlip.addEventListener('click', function() {
   triggerFlip.classList.toggle('flipped')
+})
+
+box2.addEventListener('click', function() {
+  box2.classList.toggle('flipped')
 })
 
 
